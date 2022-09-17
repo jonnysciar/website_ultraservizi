5017b21250ec49c84ac503ec02bae6f6aa03d06d
Jonathan Sciarrabba <sciarrabba.j00@gmail.com>
2022-09-17 19:02:19 +0200

    Fix build process

--- react_frontend/src/index.js
+++ react_frontend/src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom/client';
 import {Page} from "./components/page";
 import theme from "./components/theme";
 import {ThemeProvider} from "@mui/material";
-import {BrowserRouter as Router} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Router>
-        <React.StrictMode>
-            <ThemeProvider theme={theme}>
-                <Page />
-            </ThemeProvider>
-        </React.StrictMode>
-    </Router>
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <Page />
+        </ThemeProvider>
+    </React.StrictMode>
 );
