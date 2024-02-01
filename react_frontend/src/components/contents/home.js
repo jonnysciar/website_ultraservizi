00fe42f02d4c14a43eb4b9b130f9f6a520872e45
Jonathan Sciarrabba <sciarrabba.j00@gmail.com>
2024-02-01 20:36:13 +0100

    Fix resizable homepage text
    Signed-off-by: Jonathan Sciarrabba <sciarrabba.j00@gmail.com>

--- react_frontend/src/components/contents/home.js
+++ react_frontend/src/components/contents/home.js
@@ -1,6 +1,7 @@
+import * as React from 'react';
 import Grid from "@mui/material/Grid";
-import * as React from "react";
 import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
 function HomeText(props) {
     return (
@@ -40,33 +41,84 @@ function HomeText(props) {
 }
 
 function HomeDesc(props) {
+    let props_lg = JSON.parse(JSON.stringify(props));
+    let props_xs = JSON.parse(JSON.stringify(props));
+    props_lg.sx.display = {xs: "none", lg: "block"};
+    props_xs.sx.display = {xs: "block", lg: "none"};
     return (
-        <Grid
-            container
+        <Container
+            disableGutters
             sx={{
                 display: "flex",
-                justifyContent: "center",
+                flexDirection: {xs: "column", lg: "row"},
+                justifyContent: {xs: "flex-start", lg: "center"},
+                alignItems: "center"
             }}
         >
+            {/* Large description */}
             <Typography
-                {...props}
+                {...props_lg}
                 color="primary.main"
             >
                 "
             </Typography>
             <Typography
-                {...props}
+                {...props_lg}
                 color="white"
             >
                 Professionalità e cura per i vostri ambienti
             </Typography>
             <Typography
-                {...props}
+                {...props_lg}
                 color="primary.main"
             >
                 "
             </Typography>
-        </Grid>
+
+            {/* Extra Small description */}
+            <Container
+                disableGutters
+                sx={{
+                    display: {xs: "inline-flex", lg: "none"},
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <Typography
+                    {...props_xs}
+                    color="primary.main"
+                >
+                    "
+                </Typography>
+                <Typography
+                    {...props_xs}
+                    color="white"
+                >
+                    Professionalità e cura<br/>
+                </Typography>
+            </Container>
+            <Container
+                disableGutters
+                sx={{
+                    display: {xs: "inline-flex", lg: "none"},
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <Typography
+                    {...props_xs}
+                    color="white"
+                >
+                    per i vostri ambienti
+                </Typography>
+                <Typography
+                    {...props_xs}
+                    color="primary.main"
+                >
+                    "
+                </Typography>
+            </Container>
+        </Container>
     )
 }
 
@@ -74,9 +126,8 @@ function Home(props) {
     return (
         <Grid
             container
-            disableGutters
             sx={{
-                height: {lg: "100vh", xs: "70vh"},
+                height: {xs: "70vh", lg: "100vh"},
                 backgroundImage: "radial-gradient(#2c2c2d 2px, transparent 0)",
                 backgroundSize: "120px 120px",
                 mb: 5
@@ -88,20 +139,19 @@ function Home(props) {
                 item
                 xs={6}
                 sx={{
-                    height: {lg: "50vh", xs: "35vh"},
-                    borderBottomRightRadius: {lg: "200px", xs: "100px"},
-                    backgroundImage: "radial-gradient(farthest-corner at 0px 0px, #ab1f24, #e61e25)"
+                    height: {xs: "35vh", lg: "50vh"},
+                    borderBottomRightRadius: {xs: "100px", lg: "200px"},
+                    backgroundImage: "radial-gradient(farthest-corner at 0px 0px, #ab1f24, #e61e25)",
                 }}
             >
                 <Grid
-                    container
                     item
                     xs={12}
                     sx={{
                         display: "flex",
                         justifyContent: "center",
                         alignItems: "flex-end",
-                        minHeight: {xs: "20vh", sm: ""}
+                        minHeight: {xs: "20vh", sm: ""},
                     }}
                 >
                     <HomeText variant="h2"
@@ -111,7 +161,7 @@ function Home(props) {
                                   fontWeight: 'bold',
                                   fontFamily: 'UltraServiziFont',
                                   fontSize: {xs: "11vw", sm: "5vw"},
-                              }} />
+                              }}/>
 
                 </Grid>
                 <Grid
@@ -129,7 +179,7 @@ function Home(props) {
                         component="p"
                         sx={{
                             fontWeight: 'bold',
-                            fontSize: "2vw",
+                            fontSize: {xs: "3vw", lg: "2vw"},
                         }}
                     />
                 </Grid>
