124441fa35bcf5e8540a53074333634c4fc181a3
Jonathan Sciarrabba <sciarrabba.j00@gmail.com>
2026-03-18 13:22:50 +0100

    react_frontend: src: components: structures: footer: Add P.IVA
    Signed-off-by: Jonathan Sciarrabba <sciarrabba.j00@gmail.com>

--- react_frontend/src/components/structures/footer.js
+++ react_frontend/src/components/structures/footer.js
@@ -11,7 +11,7 @@ function Copyright(props) {
             align='center'
             {...props}
         >
-            {'Copyright ©  ' + new Date().getFullYear() + ' UltraServizi.'}
+            {'Copyright ©  ' + new Date().getFullYear() + ' Ultra Servizi di Salvatorico Sciarrabba - P.IVA 0123456789'}
         </Typography>
     );
 }
